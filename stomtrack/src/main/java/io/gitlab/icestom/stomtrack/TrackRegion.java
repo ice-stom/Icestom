a6b877e711d68870bffd8765567f2491d91abd07
micowavedram <microwavedram@gmail.com>
2026-07-26 17:26:02 +0100

    add inverted cuboid and fix naming

--- stomtrack/src/main/java/io/gitlab/icestom/stomtrack/TrackRegion.java
+++ stomtrack/src/main/java/io/gitlab/icestom/stomtrack/TrackRegion.java
@@ -9,10 +9,12 @@ import java.util.List;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = TrackRegion.CuboidCheckpoint.class, name = "cuboid"),
+        @JsonSubTypes.Type(value = TrackRegion.CuboidRegion.class, name = "cuboid"),
+        @JsonSubTypes.Type(value = TrackRegion.InvertedCuboidRegion.class, name = "inverted_cuboid"),
         @JsonSubTypes.Type(value = TrackRegion.PolyRegion.class, name = "poly"),
 })
 public interface TrackRegion {
-    record CuboidCheckpoint(Vec3 min, Vec3 max) implements TrackRegion {}
+    record CuboidRegion(Vec3 min, Vec3 max) implements TrackRegion {}
+    record InvertedCuboidRegion(Vec3 min, Vec3 max) implements TrackRegion {}
     record PolyRegion(List<Vec2> vec2s, double yMin, double yMax) implements TrackRegion {}
 }

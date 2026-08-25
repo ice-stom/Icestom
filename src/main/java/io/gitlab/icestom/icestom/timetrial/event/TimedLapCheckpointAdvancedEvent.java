316368139fce36f196207b4f7076f6ba74919eba
sarukz <sarukz.online@gmail.com>
2026-08-25 22:40:58 +0200

    Added checkpoint notifications

---
+++ src/main/java/io/gitlab/icestom/icestom/timetrial/event/TimedLapCheckpointAdvancedEvent.java
@@ -0,0 +1,27 @@
+package io.gitlab.icestom.icestom.timetrial.event;
+
+import io.gitlab.icestom.icestom.timetrial.TimeTrialingInstance;
+import io.gitlab.icestom.icestom.timetrial.lap.TimedLap;
+import net.minestom.server.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class TimedLapCheckpointAdvancedEvent implements TimedLapEvent {
+    @NotNull private final TimedLap lap;
+    @NotNull private final Player player;
+
+    public TimedLapCheckpointAdvancedEvent(@NotNull TimedLap lap, @NotNull Player player) {
+        this.lap = lap;
+        this.player = player;
+    }
+
+    @Override
+    public @NotNull TimedLap getLap() {
+        return lap;
+    }
+
+    @Override
+    public @NotNull Player getPlayer() {
+        return player;
+    }
+
+}
